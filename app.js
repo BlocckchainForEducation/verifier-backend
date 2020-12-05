a38eaab9934ed6487882ceab096ab568bb849b54
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-05 20:03:21 +0700

    :sparkles: add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+require("dotenv").config();
+
+const cors = require("cors");
+app.use(cors());
+
+const decoder = require("./routes/decode-token");
+app.use(decoder);
+
+const checker = require("./routes/check-integrity");
+app.use(checker);
+
+app.listen(8002, () => {
+  console.log("App listening on port 8002!");
+});
