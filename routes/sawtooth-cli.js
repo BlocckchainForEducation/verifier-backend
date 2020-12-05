77b1c0e159ae29357eaf264691949abe536f4334
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-05 20:04:03 +0700

    :heavy_plus_sign: mockup sawtooth-cli

---
+++ routes/sawtooth-cli.js
@@ -0,0 +1,10 @@
+// TODO: implement here
+async function getTxInfo(blockid, txid, address) {
+  return {
+    ok: true, // false if tx does not exists!
+    cipher: "asdf",
+    timestamp: 12341234,
+  };
+}
+
+module.exports = { getTxInfo };
