1ba451c0ff8699f8d42f29720d6d923be63104dc
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-16 15:24:34 +0700

    :pencil2:update check-integrity

--- routes/check-integrity.js
+++ routes/check-integrity.js
@@ -1,36 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const sawtoothCli = require("./sawtooth-cli");
+const axios = require("axios").default;
+const ecies = require("ecies-geth");
 
 router.post("/check-integrity", async (req, res) => {
   try {
-    // const body = req.body;
-    // const txInfo = await sawtoothCli.getTxInfo(body.txid, body.address);
-    // if (!txInfo.ok) return res.json({ valid: false, msg: "The tx does not exists!" });
+    const publicKeyHex65 = req.body.publicKeyHex65;
+    const plain = req.body.plain;
+    const txid = req.body.txid;
 
-    // if (checkIntegrity(req.body.plain, txInfo.cipher, req.body.publicKeyHex)) {
-    //   res.json({ valid: true, timestamp: txInfo.timestamp });
-    // } else {
-    //   res.json({ valid: false, msg: "Not integrity!" });
-    // }
-
-    const randTime = 1500 + Math.floor(Math.random() * 1500);
-    const randErr = 1500 + Math.floor(Math.random() * 1500);
-    const inValid = randTime % 8 == 0;
-    const randomErrMsg = randErr % 2 == 0 ? "Not integrity!" : "Not found tx!";
-    if (inValid) {
-      setTimeout(() => res.json({ valid: false, msg: randomErrMsg }), randTime);
-    } else {
-      setTimeout(() => res.json({ valid: true, timestamp: Date.now() }), randTime);
+    try {
+      const response = await axios.get("/record/" + txid);
+      if (response.data.ok) {
+        const cipherOnBkc = response.data.cipher;
+        const timestamp = response.data.timestamp;
+        if (await checkIntegrity(plain, cipherOnBkc, publicKeyHex65)) {
+          res.json({ valid: true, timestamp: timestamp });
+        } else {
+          res.json({ valid: false, msg: "Not integrity!" });
+        }
+      } else {
+        res.json({ valid: false, msg: "The tx does not exists!" });
+      }
+    } catch (error) {
+      if (error.response) return res.status(502).json({ msg: error.response.data.error });
+      else return res.status(500).json(error);
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.toString());
   }
 });
 
-// TODO: implement check here
-function checkIntegrity(plain, cipher, publicKeyHex) {
-  return true;
+async function checkIntegrity(plain, cipherOnBkc, publicKeyHex65) {
+  const cipher = (await ecies.encrypt(Buffer.from(publicKeyHex65, "hex"), Buffer.from(JSON.stringify(plain)))).toString("hex");
+  return cipher === cipherOnBkc;
 }
 
 module.exports = router;
