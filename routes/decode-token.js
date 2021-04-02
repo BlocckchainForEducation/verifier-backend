a6b8962f9d6e674b30c753c62c4f48e580b20df1
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-04-02 11:23:53 +0700

    :construction: start working v1.2

--- routes/decode-token.js
+++ routes/decode-token.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 router.post("/decode-token", async (req, res) => {
   try {
     const decodedData = jwt.verify(req.body.token, process.env.TOKEN_SECRET);
-    res.json(decodedData);
+    return res.json(decodedData);
   } catch (error) {
     res.status(400).json(error);
   }
