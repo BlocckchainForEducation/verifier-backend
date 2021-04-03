9187fe9dcf700da3bb40756f57c2857e5feb6ed2
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-04-03 19:38:16 +0700

    :pencil2: change how check integrity

--- routes/check-integrity.js
+++ routes/check-integrity.js
@@ -4,7 +4,7 @@ const { hashObject } = require("./utils");
 
 router.post("/check-integrity", async (req, res) => {
   try {
-    // console.log({ plainHash: hashObject(req.body.plain), bkchash: req.body.hash });
+    console.log({ plainHash: hashObject(req.body.plain), bkchash: req.body.hash });
     return res.json({ isIntegrity: hashObject(req.body.plain) === req.body.hash });
   } catch (error) {
     console.error(error);
