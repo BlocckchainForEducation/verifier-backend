d9b494106e96e73c6d0366d3ccda9c4fd8467e99
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-04-15 17:20:07 +0700

    :whale: dockerized

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+// require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` }); docker compose env_file will provide env variable
 const axios = require("axios").default;
-axios.defaults.baseURL = process.env.REST_API_URL;
+// axios.defaults.baseURL = process.env.REST_API_URL;
+axios.defaults.baseURL = process.env.STUDENT_API_PROVIDER;
 
 const cors = require("cors");
 app.use(cors());
@@ -13,6 +14,8 @@ app.use(require("./routes/decode-token"));
 app.use(require("./routes/check-integrity"));
 app.use(require("./routes/fetch-encrypt-data"));
 
-app.listen(8003, () => {
-  console.log("App listening on port 8003!");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
